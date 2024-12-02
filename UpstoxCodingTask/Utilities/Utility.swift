a866045212fd0c8c80c503664f0f0ae4a24a6fb5
purnachandraobulasetty <purnachandra.tech@gmail.com>
2024-12-02 10:22:42 +0530

    Removed comments

--- UpstoxCodingTask/Utilities/Utility.swift
+++ UpstoxCodingTask/Utilities/Utility.swift
@@ -25,22 +25,3 @@ final class Utility {
     }
 }
 
-/*
-final class Utility {
-
-    static func showAlert(for error: NSError,withPresenter presenter: UIViewController) {
-        // Present the alert on the main thread
-        let alert = UIAlertController(title: error.domain, message: error.localizedDescription, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-        presenter.present(alert, animated: true, completion: nil)
-    }
-    
-    static func showAlert(title:String,message:String,withPresenter presenter: UIViewController) {
-        // Present the alert on the main thread
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-        presenter.present(alert, animated: true, completion: nil)
-    }
-}
-*/
-
